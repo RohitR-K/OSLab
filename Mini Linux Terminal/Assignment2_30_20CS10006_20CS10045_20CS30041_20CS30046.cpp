84f0f6b59916200a3f4f4ce9dc71d0f9e5f0fca0
RohitR-K <124794890+RohitR-K@users.noreply.github.com>
2023-07-09 21:34:45 +0530

    Add files via upload

---
+++ Mini Linux Terminal/Assignment2_30_20CS10006_20CS10045_20CS30041_20CS30046.cpp
@@ -0,0 +1,10 @@
+•  Pioneered the development of MyTCP, a sophisticated message oriented TCP protocol that cobines the reliability and in-order delivery
+
+•  Programming and Data Structures*            •  Computer Networks*                         •  Linear Algebra for AI & ML
+•  Algorithms - 1*, 2                          •  Operating Systems*                         •  Machine Learning
+•  Systems Programming Lab                     •  Compilers*                                 •  Deep Learning
+•  Software Engineering*                       •  Database Management Systems*               •  Probability and Statistics
+•  Discrete Structures                         •  Computer Organization & Architecture*      •  Signals & Systems
+•  Formal Language & Automata Theory           •  Switching Circuits & Logic Design*         •  Basic Electronics*
+
+• Software & Tools: Bash, Linux Shell, VS Code, Google Colab, MS Office                             •  Databases - MySql, PostgreSQL*
